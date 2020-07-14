6508ddfbcca041d95fe85064e93b9081da1766ce
Elliot Parker <elliotccp@protonmail.com>
2020-07-14 10:35:33 +1000

    Fix average calculation

--- AverageContinuous.java
+++ AverageContinuous.java
@@ -1,6 +1,6 @@
 /**
  * @Elliot Parker
- * @v1.0.0-2020-07-14
+ * @v1.3.2-2020-07-14
  */
 
 import java.util.Scanner;
@@ -11,17 +11,17 @@ public class AverageContinuous
     int i;
     int current;
     int x;
-    int avg;
+    float avg;
     public AverageContinuous()
     {
         System.out.print("Input numbers (-1 to stop): ");
-        for(i = 1;current != -1; i++)
+        for(i = -1;current != -1; i++)
         {
             x += current;
             Scanner input = new Scanner(System.in);
             current = input.nextInt();
         }
-        avg = Math.round((i)/x);
+        avg = (float)x/i;
         System.out.print(avg);
     }
 }
